9296ea4a7879b67e36b7c6e351cf8cf9b6c3232e
Yunfei He <i.heyunfei@gmail.com>
2022-02-27 22:58:26 +0800

    Tweak

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  redirects: [
-    {
-      source: '/',
-      destination: `/${process.env.NOTION_PAGE_ID}`,
-      permanent: true,
-    },
-  ],
 }
 
 module.exports = nextConfig
